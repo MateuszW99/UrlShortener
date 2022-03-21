13af641035349b4a9464eefcba6f7f92ce95024c
Mateusz Wiczuk <mwiczuk99@gmail.com>
2022-03-21 20:22:27 +0100

    Add IApplicationDbContext

---
+++ src/UrlShortener.Persistence/ApplicationDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using UrlShortener.Domain.Entities;
+
+namespace UrlShortener.Persistence
+{
+    public interface IApplicationDbContext
+    {
+        DbSet<UrlShort> UrlShorts { get; set; }
+    }
+    
+    public class ApplicationDbContext : DbContext, IApplicationDbContext
+    {
+        public DbSet<UrlShort> UrlShorts { get; set; }
+        
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
