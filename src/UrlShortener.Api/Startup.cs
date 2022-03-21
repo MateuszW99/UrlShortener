95e955c5c3981ab4bafa523dd0fa322b3e30d948
Mateusz Wiczuk <mwiczuk99@gmail.com>
2022-03-21 20:23:04 +0100

    Install services

--- src/UrlShortener.Api/Startup.cs
+++ src/UrlShortener.Api/Startup.cs
@@ -8,6 +8,7 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using UrlShortener.Application;
 
 namespace Api
 {
@@ -23,6 +24,10 @@ namespace Api
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddControllers();
+
+            services.AddInMemoryDbContext();
+            
             services.AddRazorPages();
         }
 
---
+++ src/UrlShortener.Application/DependencyInjection.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using UrlShortener.Persistence;
+
+namespace UrlShortener.Application
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddInMemoryDbContext(this IServiceCollection services)
+        {
+            services.AddDbContext<ApplicationDbContext>(options =>
+            {
+                options.UseInMemoryDatabase("UrlShortenerDB");
+            });
+            
+            return services;
+        }
+    }
+}
